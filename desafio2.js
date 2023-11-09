c5d81350a7dfb7f6d9d6fa90ee18e9d198b7d9dc
MarianaPadelin <marianapadelin@gmail.com>
2023-11-09 10:36:00 -0300

    correcciones

--- desafio2.js
+++ desafio2.js
@@ -6,7 +6,8 @@ class ProductManager {
   constructor() {
     this.path = "./datos.json";
     try {
-      this.products = JSON.parse(this.products);
+      let products = fs.readFileSync(this.path, "utf-8");
+      this.products = JSON.parse(products);
     } catch {
       this.products = [];
     }
@@ -14,6 +15,7 @@ class ProductManager {
 
   async addProduct(product) {
     try {
+      //falta leer los archivos existentes
       let existeCodigo = false;
       this.products.forEach((prod) => {
         prod.code.includes(product.code)
@@ -54,7 +56,11 @@ class ProductManager {
   }
 
   getProducts() {
-    console.log(this.products);
+    return this.products;
+    //   let info = fs.readFileSync(this.path, "utf-8")
+    //  this.info = JSON.stringify(info)
+    //  console.log(this.info);
+    // console.log(this.products);
   }
 
   getProductById(idProducto) {
@@ -70,26 +76,26 @@ class ProductManager {
   }
 
   async updateProduct(idProducto, key, newValue) {
-     let productoAmodificar = this.products.find(
-       (producto) => producto.id === idProducto
-     );
-    if (!productoAmodificar){
-      console.log("Error. El producto no existe.")
-      return}
-    
-      productoAmodificar[key] = newValue
+    let productoAmodificar = this.products.find(
+      (producto) => producto.id === idProducto
+    );
+    if (!productoAmodificar) {
+      console.log("Error. El producto no existe.");
+      return;
+    }
 
-    this.deleteProduct(idProducto);
-      await fs.promises.writeFile(
-        this.path,
-        JSON.stringify(this.products, null, "\t")
-      )
+    productoAmodificar[key] = newValue;
 
+    await fs.promises.writeFile(
+      this.path,
+      JSON.stringify(this.products, null, "\t")
+    );
+    console.log("Producto actualizado");
   }
 
   async deleteProduct(idProducto) {
     const productoEncontrado = this.products.find(
-      (prod) => (prod.id) === idProducto
+      (prod) => prod.id === idProducto
     );
 
     if (!productoEncontrado) {
@@ -97,12 +103,13 @@ class ProductManager {
     }
 
     try {
-      this.products.splice(idProducto, 1);
-      console.log("Producto borrado");
+      this.products.splice(idProducto - 1, 1);
+
       await fs.promises.writeFile(
         this.path,
         JSON.stringify(this.products, null, "\t")
       );
+      console.log("Producto borrado");
     } catch (error) {
       console.log(`Hubo un error al guardar los datos: ${error}`);
       return;
@@ -126,8 +133,7 @@ class Product {
 // 1) Instancio la clase
 const prueboProducto = new ProductManager();
 
-
-// 2) Agrego productos y pruebo las validaciones. Si hay campos vacios o si se repite el código, no se agregan 
+// 2) Agrego productos y pruebo las validaciones. Si hay campos vacios o si se repite el código, no se agregan
 prueboProducto.addProduct(
   new Product(
     "Producto prueba1",
@@ -182,19 +188,14 @@ prueboProducto.addProduct(
   )
 );
 
-
-
 // 3) Busco productos por id
 prueboProducto.getProductById(2);
 
-// 4) Borro productos por id
+// // 4) Borro productos por id
 prueboProducto.deleteProduct(2);
 
-
-// 5) Actualizo productos por parámetros (id, llave, valor a modificar)
-prueboProducto.updateProduct(1, 'price', '300');
-
+// // 5) Actualizo productos por parámetros (id, llave, valor a modificar)
+prueboProducto.updateProduct(1, "price", "300");
 
 // 6) Por último obtengo el array de productos final
-prueboProducto.getProducts();
-
+console.log(prueboProducto.getProducts())
